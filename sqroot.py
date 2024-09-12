d9cf9da9b5413a26d52138b016a351816b80649d
tanayachavan2 <tchavan367@gmail.com>
2024-09-12 14:19:40 +0530

    Update sqroot.py

--- sqroot.py
+++ sqroot.py
@@ -3,3 +3,16 @@
 number=float(input("enter the number:"))
 number_new=number**2
 print("the square of the number is:{1}".format(number, number_new))
+
+# to find the square root of the given number.
+
+import math
+variable=49
+root=math.sqrt(variable)
+print(f"the square root of the given number is:{root} ")
+
+
+#OR
+
+import math
+print(math.sqrt(25))
