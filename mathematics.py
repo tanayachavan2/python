84f020bb7c3a3b6f719d1d77b2f7d3e5108d3659
tanayachavan2 <tchavan367@gmail.com>
2024-09-12 14:49:01 +0530

    Create mathematics.py

---
+++ mathematics.py
@@ -0,0 +1,19 @@
+#calculate the square of 5
+print(5**2)
+
+#print the area of circle with radius 5
+print(2*3.14*5)
+
+#this program is to add the two numbers.
+
+num1=10
+num2=5
+c=num1+num2
+print('the sum is {0}'.format(c))
+
+#to calculate the sum of two number by converting into floating point number.
+
+a=input("enter the first number")
+b=input("enter the second number")
+c=float(a)+float(b)
+print("the sum of {0} and {1} is {2}".format(a ,b ,c))
