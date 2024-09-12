06c3194794e827be78ff58d5cfd2040c387e2d32
tanayachavan2 <tchavan367@gmail.com>
2024-09-12 13:44:21 +0530

    keyword.py
    use of AND and OR keyword

---
+++ Keyword.py
@@ -0,0 +1,17 @@
+#use of AND and OR keyword
+
+#use of AND keyword
+age=25
+result= age<20 and age>22    #here 1st condition is false and 2nd is true therefore acc to table final o/p is false.
+positive=age<28 and age>24    #here 1st condition is true and 2nd is also true therefore the o/p is also true.
+
+print(result)
+print(positive)
+
+
+#use of OR keyword
+default_age=20
+user=0
+
+ouput=user or default_age
+print(output)
